507f86be067af24dd0ea1363c6a3a6aa96832d79
Jon Mico <jonmico@gmail.com>
2024-08-21 23:39:28 -0400

    Update GetAllEvents function.

--- main.go
+++ main.go
@@ -19,7 +19,14 @@ func main() {
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "could not fetch events. try again later",
+		})
+		return
+	}
 	context.JSON(http.StatusOK, events)
 }
 
@@ -36,7 +43,14 @@ func createEvent(c *gin.Context) {
 	event.ID = 1
 	event.UserID = 1
 
-	event.Save()
+	err = event.Save()
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "could not create event. try again later",
+		})
+		return
+	}
 
 	c.JSON(http.StatusCreated, gin.H{"message": "event created", "event": event})
 }
