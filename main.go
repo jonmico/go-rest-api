4c4690bcb3cf17467b477940ffef6ba24520fd90
Jon Mico <jonmico@gmail.com>
2024-08-22 09:35:51 -0400

    Add GetEventById function to model package.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"example.com/rest-api/db"
 	"example.com/rest-api/models"
@@ -13,6 +14,7 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
 
 	server.Run(":8080")
@@ -27,7 +29,20 @@ func getEvents(context *gin.Context) {
 		})
 		return
 	}
+
 	context.JSON(http.StatusOK, events)
+}
+
+func getEvent(c *gin.Context) {
+	id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "could not parse event id",
+		})
+		return
+	}
+
 }
 
 func createEvent(c *gin.Context) {
