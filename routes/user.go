c79a63a71f31c38a6681ed632ee9ca239b362a49
Jon Mico <jonmico@gmail.com>
2024-08-25 13:19:40 -0400

    Add signup and save for users.

---
+++ routes/user.go
@@ -0,0 +1,28 @@
+package routes
+
+import (
+	"net/http"
+
+	"example.com/rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func signup(c *gin.Context) {
+	var user models.User
+
+	err := c.ShouldBindBodyWithJSON(&user)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request data"})
+		return
+	}
+
+	err = user.Save()
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "could not create user"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "user created", "user": user})
+}
