20244320591ff0d787c00d183e41f06b32aaf125
Jon Mico <jonmico@gmail.com>
2024-08-25 11:58:49 -0400

    Add users table.

--- db/db.go
+++ db/db.go
@@ -24,6 +24,21 @@ func InitDB() {
 }
 
 func createTables() {
+	createUsersTable := `
+		CREATE TABLE IF NOT EXISTS users (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			email TEXT NOT NULL UNIQUE,
+			password TEXT NOT NULL
+		)
+	`
+
+	_, err := DB.Exec(createUsersTable)
+
+	if err != nil {
+		fmt.Println(err)
+		panic("could not create users table")
+	}
+
 	createEventsTable := `
 		CREATE TABLE IF NOT EXISTS events (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -35,7 +50,7 @@ func createTables() {
 		)
 	`
 
-	_, err := DB.Exec(createEventsTable)
+	_, err = DB.Exec(createEventsTable)
 
 	if err != nil {
 		fmt.Println(err)
