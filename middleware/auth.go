ff6c2a23b4408a201357a85d2058933edeb2b341
Jon Mico <jonmico@gmail.com>
2024-09-08 13:15:41 -0400

    Add Authenticate middleware.

---
+++ middleware/auth.go
@@ -0,0 +1,26 @@
+package middleware
+
+import (
+	"net/http"
+
+	"example.com/rest-api/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func Authenticate(c *gin.Context) {
+	token := c.Request.Header.Get("Authorization")
+
+	if token == "" {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "not authorized"})
+		return
+	}
+
+	userId, err := utils.VerifyToken(token)
+
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "not authorized"})
+		return
+	}
+
+	c.Next()
+}
